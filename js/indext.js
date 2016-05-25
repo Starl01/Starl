2f36e4f56004c74e433c93f8537d87da047fefa2
Starl01 <Starl01@outlook.com>
2016-05-25 11:54:44 +0800

    Update indext.js

--- js/indext.js
+++ js/indext.js
@@ -15,28 +15,6 @@ function getPos(obj){
 function a2r(ang) {
 	return ang * Math.PI / 180;
 }
-var oHero=document.getElementById('hero');
-var oHeropicul=oHero.getElementsByTagName('ul')[0];
-var aHeropiculli=oHeropicul.children;
-var oHeroLeft=document.getElementById('heroleft');
-var oHeroRight=document.getElementById('heroright');
-var oHeadNav=document.getElementById('home').getElementsByTagName('nav')[0];
-var aHeroliImg=oHeropicul.getElementsByTagName('img');
-var cHeight=document.documentElement.clientHeight;
-var oSkillicon=document.getElementById('skillicon');
-var aSkillsdiv=oSkillswrapper.children;
-var oSkillswrapper=document.getElementById('skillswrapper');
-var Skillswrappertop=getPos(oSkillswrapper).top;
-var arr=["ajax、jsonp","mvc","html5、css3","canvas","seajs","php、nodejs","git、svn","grunt、gulp"];
-var oDiv = document.getElementById('mskillsdiv');
-var oSkills=document.getElementById('mskills');
-var oBtn = oDiv.children[0];
-var count = 8;
-var oDivtop=getPos(oDiv).top;
-var bl = true;
-var oTp=document.getElementById('tupian');
-var tph=oTp.offsetHeight;
-var oTptop=getPos(oTp).top;
 window.onload=function(){
 //homeliclick
 	var aNavli=document.querySelectorAll('#home nav .navol li');
@@ -76,41 +54,8 @@ window.onload=function(){
 	});
 	aNavli[4].addEventListener("touchstart",function(ev){
 		window.smoothScroll(oContact);	
-	},false);
-//skillsclick
-function setPos(obj,ang) {
-	var a = Math.sin(a2r(ang)) * oDiv.offsetWidth / 2;
-	var b = Math.cos(a2r(ang)) * oDiv.offsetHeight / 2;
-	obj.style.left = oDiv.offsetLeft + oDiv.offsetWidth / 2 + a + 'px';
-	obj.style.top = oDiv.offsetTop + oDiv.offsetHeight / 2 - b + 'px';
-}
-function move1(obj,iTarget,fn) {
-	var start = obj.radian;
-	var dis = iTarget - start;
-	var count = Math.round(700 / 30);
-	var n = 0;
-	clearInterval(obj.timer);
-	obj.timer = setInterval(function() {
-	  n++;
-	  //cur=start+dis/count*n;
-	  var a = 1 - n / count;
-	  var cur = start + dis * (1 - a * a * a);
-	  setPos(obj,cur);
-	  obj.radian = cur;
-	  if (n == count) {
-		  fn&&fn();
-		  clearInterval(obj.timer);
-	  }
-	}, 30);
-}
-for (var i = 0; i < count; i++) {
-	var oBox = document.createElement('div');
-	oBox.innerHTML=arr[i];
-	oBox.className ='sBox';
-	oSkills.appendChild(oBox);
-	setPos(oBox,0); //定位小圆到弧线上
-	oBox.radian = 0;
-}
+	},false);	
+/*skillsclick*/
 	oBtn.onclick = function() {
 		var aBox = document.getElementsByClassName('sBox');
 		if (bl) {
