5c061a77b8068c0ad501f912566e725ff6e480e4
Starl01 <Starl01@outlook.com>
2016-05-25 11:42:57 +0800

    Update indext.js

--- js/indext.js
+++ js/indext.js
@@ -27,6 +27,16 @@ var oSkillicon=document.getElementById('skillicon');
 var aSkillsdiv=oSkillswrapper.children;
 var oSkillswrapper=document.getElementById('skillswrapper');
 var Skillswrappertop=getPos(oSkillswrapper).top;
+var arr=["ajax、jsonp","mvc","html5、css3","canvas","seajs","php、nodejs","git、svn","grunt、gulp"];
+var oDiv = document.getElementById('mskillsdiv');
+var oSkills=document.getElementById('mskills');
+var oBtn = oDiv.children[0];
+var count = 8;
+var oDivtop=getPos(oDiv).top;
+var bl = true;
+var oTp=document.getElementById('tupian');
+var tph=oTp.offsetHeight;
+var oTptop=getPos(oTp).top;
 window.onload=function(){
 //homeliclick
 	var aNavli=document.querySelectorAll('#home nav .navol li');
@@ -66,8 +76,41 @@ window.onload=function(){
 	});
 	aNavli[4].addEventListener("touchstart",function(ev){
 		window.smoothScroll(oContact);	
-	},false);	
-/*skillsclick*/
+	},false);
+//skillsclick
+function setPos(obj,ang) {
+	var a = Math.sin(a2r(ang)) * oDiv.offsetWidth / 2;
+	var b = Math.cos(a2r(ang)) * oDiv.offsetHeight / 2;
+	obj.style.left = oDiv.offsetLeft + oDiv.offsetWidth / 2 + a + 'px';
+	obj.style.top = oDiv.offsetTop + oDiv.offsetHeight / 2 - b + 'px';
+}
+function move1(obj,iTarget,fn) {
+	var start = obj.radian;
+	var dis = iTarget - start;
+	var count = Math.round(700 / 30);
+	var n = 0;
+	clearInterval(obj.timer);
+	obj.timer = setInterval(function() {
+	  n++;
+	  //cur=start+dis/count*n;
+	  var a = 1 - n / count;
+	  var cur = start + dis * (1 - a * a * a);
+	  setPos(obj,cur);
+	  obj.radian = cur;
+	  if (n == count) {
+		  fn&&fn();
+		  clearInterval(obj.timer);
+	  }
+	}, 30);
+}
+for (var i = 0; i < count; i++) {
+	var oBox = document.createElement('div');
+	oBox.innerHTML=arr[i];
+	oBox.className ='sBox';
+	oSkills.appendChild(oBox);
+	setPos(oBox,0); //定位小圆到弧线上
+	oBox.radian = 0;
+}
 	oBtn.onclick = function() {
 		var aBox = document.getElementsByClassName('sBox');
 		if (bl) {
