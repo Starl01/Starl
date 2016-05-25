d4dac8419c45170e9b01dc970afb60612ccc4a22
Starl01 <Starl01@outlook.com>
2016-05-25 12:17:07 +0800

    Update indexb.js

--- js/indexb.js
+++ js/indexb.js
@@ -62,3 +62,115 @@ for (var i = 0; i < count; i++) {
 var oTp=document.getElementById('tupian');
 var tph=oTp.offsetHeight;
 var oTptop=getPos(oTp).top;
+function getPos(obj){
+	var l=t=0;
+	var i=0;
+	while(obj){
+		l+=obj.offsetLeft;
+		t+=obj.offsetTop;
+		obj=obj.offsetParent;	
+		i++;
+	}
+	
+	return {left:l, top:t};
+}
+
+function a2r(ang) {
+	return ang * Math.PI / 180;
+}
+//homeliclick
+	var aNavli=document.querySelectorAll('#home nav .navol li');
+	var oSkillcontainer=document.querySelector('.skills .container');
+	var oWorksul=document.getElementById('worksul');
+	var oWimg=document.getElementById('worksulimg');
+	var aWorkulwp4=document.querySelectorAll('#worksul .wp4');
+	var oWmore=document.getElementById('worksmore');
+	var oWbtn=document.getElementById('wmorebtn');
+	var oWdestination = document.querySelector('#worksul2');
+	var oHeadingmyworksbtn=document.querySelector('.down-arrow-btn');
+	var oWorkscontainer=document.querySelector('#works .container');
+	var oContactuptopbtn=document.querySelector('.up-btn');
+	var oHome=document.querySelector('#home');
+	var oAboutme=document.querySelector('.aboutme');
+	var oContact=document.querySelector('#contact');
+	aNavli[1].addEventListener('click', function(ev){
+			window.smoothScroll(oSkillcontainer);	
+	});
+	aNavli[1].addEventListener("touchstart",function(ev){
+		window.smoothScroll(oSkillcontainer);	
+	});
+	aNavli[2].addEventListener('click', function(ev){
+		window.smoothScroll(oWorkscontainer);	
+	});
+	aNavli[2].addEventListener("touchstart",function(ev){
+		window.smoothScroll(oWorkscontainer);	
+	},false);	
+	aNavli[3].addEventListener('click', function(ev){
+		window.smoothScroll(oAboutme);	
+	});
+	aNavli[3].addEventListener("touchstart",function(ev){
+		window.smoothScroll(oAboutme);	
+	},false);	
+	aNavli[4].addEventListener('click', function(ev){
+		window.smoothScroll(oContact);	
+	});
+	aNavli[4].addEventListener("touchstart",function(ev){
+		window.smoothScroll(oContact);	
+	},false);	
+/*skillsclick*/
+	oBtn.onclick = function() {
+		var aBox = document.getElementsByClassName('sBox');
+		if (bl) {
+			
+			for (var i = 0; i < aBox.length; i++) {
+			
+				aBox[i].style.display="block";
+				aBox[i].style.background="rgba("+parseInt(Math.random()*255)+","+parseInt(Math.random()*255)+","+parseInt(Math.random()*255)+","+0.8+")";
+				//move1(aBox[i], i * (90 / (count - 1))); //0	0*90/6
+				move1(aBox[i], i * (360 / count));
+			}
+			bl = false; //45				180
+			
+		} else { //60				240
+			for (var i = 0; i < aBox.length; i++) {
+				//75 300
+				move1(aBox[i],0);
+			}
+			setTimeout(function () {
+				for (var i = 0; i < aBox.length; i++) {
+					aBox[i].style.display="none";
+				}
+			},700)
+									
+			bl = true;
+		}
+	}
+	//headingmyworks
+	oHeadingmyworksbtn.addEventListener('click', function(ev){
+		window.smoothScroll(oWorkscontainer);	
+	});
+	oHeadingmyworksbtn.addEventListener("touchstart",function(ev){
+		window.smoothScroll(oWorkscontainer);	
+	},false);	
+	
+	//works
+	var handleClick = function(ev) {
+		//alert(getPos(oWdestination).top);
+		var oEvent=ev||event;
+	    oEvent.preventDefault();
+		oWmore.className='worksmore workshover';
+		oWmore.style.height=oWorksul.offsetHeight+'px';
+	  	window.smoothScroll(oWdestination);
+		
+	};
+	oWbtn.addEventListener('click', handleClick);
+	oWbtn.addEventListener("touchstart",function(ev){
+		handleClick(ev);
+	},false);	
+	//contact
+	oContactuptopbtn.addEventListener('click', function(ev){
+		window.smoothScroll(oHome);	
+	});
+	oContactuptopbtn.addEventListener("touchstart",function(ev){
+		window.smoothScroll(oHome);	
+	},false);	
